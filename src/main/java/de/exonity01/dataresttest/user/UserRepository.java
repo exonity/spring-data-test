1b63788b40f339c95800f1ec2cae508e5f461c44
markus <mangei.markus@googlemail.com>
2019-03-28 17:01:55 +0100

    Added projection

--- src/main/java/de/exonity01/dataresttest/user/UserRepository.java
+++ src/main/java/de/exonity01/dataresttest/user/UserRepository.java
@@ -1,8 +1,16 @@
 package de.exonity01.dataresttest.user;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource(collectionResourceRel = "user", path = "user")
 public interface UserRepository extends PagingAndSortingRepository<User, Long> {
+
+    @Query
+    public Page<User> findByNameContains(@Param("name") String name, Pageable pageable);
+
 }
