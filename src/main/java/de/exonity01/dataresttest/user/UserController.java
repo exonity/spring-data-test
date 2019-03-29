eb42559cb6e2cdabecd94fc43dae7a2fb0891031
markus <mangei.markus@googlemail.com>
2019-03-29 17:25:42 +0100

    ...

--- src/main/java/de/exonity01/dataresttest/user/UserController.java
+++ src/main/java/de/exonity01/dataresttest/user/UserController.java
@@ -1,19 +1,23 @@
 package de.exonity01.dataresttest.user;
 
 
+import com.querydsl.core.types.OrderSpecifier;
 import com.querydsl.core.types.Projections;
 import com.querydsl.core.types.dsl.StringExpression;
 import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.persistence.EntityManager;
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -24,7 +28,7 @@ public class UserController {
     @Autowired
     private EntityManager entityManager;
     @Autowired
-    private  UserService userService;
+    private UserService userService;
     @Autowired
     private UserRepository userRepository;
 
@@ -45,5 +49,63 @@ public class UserController {
     public Page<UserTableProjection2> getAll2(Pageable pageable) {
         return userRepository.findAll2(pageable);
     }
+
+
+///////////////////////////////////////
+
+    @GetMapping("new")
+    public ResponseEntity getAll(UserSearchCriteria criteria, Pageable pageable) {
+        // QueryFactory
+        JPAQueryFactory jpaQueryFactory = new JPAQueryFactory(entityManager);
+
+        // Query
+        QUser qUser = QUser.user;
+        StringExpression exp = qUser.name.concat(" ").concat(qUser.surname);
+        JPAQuery<UserTableProjection> query = jpaQueryFactory
+                .select(Projections.fields(UserTableProjection.class, exp.as("nameSurname")))
+                .from(qUser);
+
+        // Offset and limit
+        query.offset(pageable.getOffset());
+        query.limit(pageable.getPageSize());
+
+        // Filter
+
+        // Order
+        convertSortToOrderSpecifier(pageable.getSort());
+        query.orderBy(convertSortToOrderSpecifier(pageable.getSort()));
+
+        // Build page
+        long rowsCount = query.fetchCount();
+        Page page = new PageImpl<>(query.fetch(), pageable, rowsCount);
+
+        // Serve
+        return ResponseEntity.ok(page);
+    }
+
+    private OrderSpecifier[] convertSortToOrderSpecifier(Sort sort) {
+        List<OrderSpecifier> orderSpecifierList = new ArrayList<>();
+        for (Sort.Order order : sort) {
+            OrderSpecifier orderSpecifier = convertOrderToOrderSpecifier(order);
+            if (orderSpecifier != null) {
+                orderSpecifierList.add(orderSpecifier);
+            }
+        }
+        return orderSpecifierList.toArray(new OrderSpecifier[orderSpecifierList.size()]);
+    }
+
+    private OrderSpecifier convertOrderToOrderSpecifier(Sort.Order order) {
+        QUser qUser = QUser.user;
+        if ("name".equals(order.getProperty())) {
+            return order.isAscending() ? qUser.name.asc() : qUser.name.desc();
+        } else if ("surname".equals(order.getProperty())) {
+            return order.isAscending() ? qUser.surname.asc() : qUser.surname.desc();
+        } else if ("nameSurname".equals(order.getProperty())) {
+            StringExpression exp = qUser.name.concat(" ").concat(qUser.surname);
+            return order.isAscending() ? exp.asc() : exp.desc();
+        }
+        return null;
+    }
+
 
 }
---
+++ src/main/java/de/exonity01/dataresttest/user/UserSearchCriteria.java
@@ -0,0 +1,14 @@
+package de.exonity01.dataresttest.user;
+
+import lombok.Data;
+
+@Data
+public class UserSearchCriteria {
+
+    private String name;
+
+    private String surname;
+
+    private String nameSurname;
+
+}
