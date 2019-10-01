9eafed7fba083d472eb64ffe5340acba10fe996a
markus <mangei.markus@googlemail.com>
2019-10-01 11:12:43 +0200

    Try to enable CustomerCreateDtoValidator

--- src/main/java/de/exonity01/dataresttest/customer/web/CustomerCreateDtoValidator.java
+++ src/main/java/de/exonity01/dataresttest/customer/web/CustomerCreateDtoValidator.java
@@ -1,21 +1,12 @@
 package de.exonity01.dataresttest.customer.web;
 
-import lombok.NonNull;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
 import org.springframework.validation.ValidationUtils;
 import org.springframework.validation.Validator;
-import org.springframework.validation.beanvalidation.SpringValidatorAdapter;
 
 @Component
 public class CustomerCreateDtoValidator implements Validator {
-
-    private final @NonNull SpringValidatorAdapter validator;
-
-    public CustomerCreateDtoValidator(SpringValidatorAdapter validator) {
-        super();
-        this.validator = validator;
-    }
 
     @Override
     public boolean supports(Class<?> clazz) {
